4d060d017d51d17905319b4578b049bb5a34e885
slobodangrbic <slobodan.grbic@codaxy.com>
2025-03-24 10:12:32 +0100

    add nginx deplyoment

---
+++ argo-helm/terraform/1-argocd.tf
@@ -0,0 +1,9 @@
+resource "helm_release" "argo" {
+  name       = "argo"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  version    = "3.35.4"
+  namespace  = "argocd"
+  create_namespace = true
+  values = [file("values/argocd.yaml")]
+}
